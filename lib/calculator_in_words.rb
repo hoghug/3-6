fb78d19f18c66c992288229560cd374ee4eac1f3
Chris Lind & KeMonté Rashad Jones <student@epicodus.com>
2014-03-06 12:13:54 -0800

    finished project

--- lib/calculator_in_words.rb
+++ lib/calculator_in_words.rb
@@ -1,26 +1,40 @@
 def calculator_in_words(evaluation)
-  final_answer = 0;
   evaluation.downcase!
   cleaned_string = evaluation.gsub(/what|is|by|the|power|rd|nd|st|th|\?|\s/, '')
   operator_string = cleaned_string.gsub(/plus|minus|divided|to|times/, 'plus' => '+', 'minus' => '-', 'times' => '*', 'divided' => '/', 'to' => '^' )
   saved_string = operator_string
-  numbers_to_evaluate = operator_string.split(/\D/)
+  numbers_to_evaluate = operator_string.split(/[^0-9\.]/)
   numbers_to_evaluate.reject! { |v| v.empty? }
-  operator_symbols = saved_string.split(/\d/).reject! { |v| v.empty? }
+  operator_symbols = saved_string.gsub(/\./,'').split(/\d/).reject! { |v| v.empty? }
 
   if operator_symbols[0] == '^'
-    result = numbers_to_evaluate[0].to_i**numbers_to_evaluate[1].to_i
+    result = numbers_to_evaluate[0].to_f**numbers_to_evaluate[1].to_f
   else
-    result = numbers_to_evaluate[0].to_i.send(operator_symbols[0],numbers_to_evaluate[1].to_i)
+    result = numbers_to_evaluate[0].to_f.send(operator_symbols[0],numbers_to_evaluate[1].to_f)
   end
 
   if numbers_to_evaluate.length > 2 
-    result = result.send(operator_symbols[1], numbers_to_evaluate[2].to_i)
+    result = result.send(operator_symbols[1], numbers_to_evaluate[2].to_f)
   end
 
-  result
-
+  trim(result)
 
 end
 
-puts calculator_in_words("What is 6 plus 8")
+def split_questions(questions)
+  answers_array = []
+  questions_array = questions.split(/(.*?)\?/).reject! { |v| v.empty? }
+  questions_array.each do |question|
+    answers_array << calculator_in_words(question)
+  end
+  answers_array
+end
+
+def trim (number)
+  i = number.to_i
+  f = number.to_f
+  i == f ? i : f
+end
+
+puts split_questions("What is 8 to 2nd power? what is 2 times 7?")
+# puts calculator_in_words("What is 6 plus 8")
--- spec/calculator_in_words_spec.rb
+++ spec/calculator_in_words_spec.rb
@@ -6,15 +6,24 @@ describe('calculator_in_words') do
     calculator_in_words('5 plus 4').should(eq(9))
   end
   it('returns the product of the inputted string') do
-    calculator_in_words("What is 3 times 2").should(eq(6))
+    calculator_in_words("What is 3 times 2?").should(eq(6))
   end
   it('returns the quotient of the inputted string') do
-    calculator_in_words("What is 12 divided by 4").should(eq(3))
+    calculator_in_words("What is 12 divided by 4?").should(eq(3))
   end
   it('returns the exponent of the inputted string') do
-    calculator_in_words("What is 4 to the 3rd power").should(eq(64))
+    calculator_in_words("What is 4 to the 3rd power?").should(eq(64))
   end
   it('returns the answer for multiple operations') do
-    calculator_in_words("What is 6 plus 8 divided by 2").should(eq(7))
+    calculator_in_words("What is 6 plus 8 divided by 2?").should(eq(7))
+  end
+  it('evaluates questions with floats') do
+    calculator_in_words("What is 7.2 plus 2.8").should(eq(10))
+  end
+end
+
+describe('split_questions') do
+  it('returns the answer for multiple questions') do
+    split_questions("What is 2 plus 2? What is 28 divided 7").should(eq([4,4]))
   end
 end
